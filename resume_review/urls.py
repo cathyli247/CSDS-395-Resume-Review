39f8dbb441829cd08920d5b5283c85d376b9ad63
shiqi.li <shiqi.li@autobizline.com>
2021-09-29 16:16:08 -0400

    implement backend for register and login

--- resume_review/urls.py
+++ resume_review/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name="register"),
-    path('login/', views.LoginView.as_view(), name="login"),
+    path('', views.LoginView.as_view(), name="login"),
+    path('home/', views.HomePageView.as_view(), name='home'),
 ]
