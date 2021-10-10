cb820bbb88a6132cf5deee2eb7200ecf5bf32627
sherrytxz06 <sherrytxz06@gmail.com>
2021-10-10 16:27:00 -0400

    User's order detail page, format needed to change

--- static/js/orderpage.js
+++ static/js/orderpage.js
@@ -3,3 +3,7 @@ const fileSelected = document.getElementById('file-selected');
 selectFileBtn.addEventListener('change', function(){
     fileSelected.textContent = this.files[0].name
 })
+
+function openRatingForm(){
+    document.body.classList.add("showRatingForm");
+}
