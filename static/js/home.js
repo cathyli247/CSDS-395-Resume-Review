671692aa098fc7b72684ad77bad40e1f72ced254
Ningjia Huang <nxh239@case.edu>
2021-10-19 15:24:03 -0400

    Fixed home page card.

---
+++ static/js/home.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    $(".card-img").parent().css({position: 'relative'});
+    $(".card-img").css({top: 0, left: 0, position:'absolute'});
+    $(".btn").parent().css({position: 'relative'});
+    $(".btn").css({top: 460, left: 120, position:'absolute'});
+    $(".profile-img").parent().css({position: 'relative'});
+    $(".profile-img").css({top: 210, left: 90, position:'absolute'});
+    $(".about").parent().css({position: 'relative'});
+    $(".about").css({top: 300, left: 10, position:'absolute'});
+    $("h1").parent().css({position: 'relative'});
+    $("h1").css({top: 260, left: 123, position:'absolute'});
+});
