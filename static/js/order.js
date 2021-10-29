5b03ad649b1be20e4ae581fd91e7e61ec2449f45
Sherry Zhao <sherrytxz06@gmail.com>
2021-10-29 17:52:39 -0400

    status color js implemented

--- static/js/order.js
+++ static/js/order.js
@@ -1,3 +1,19 @@
 $( document ).ready(function() {
     $("li#order" ).addClass("active");
 });
+
+$( document ).ready(function() {
+    if ($('#status-of-order').text().indexOf('Pending') > -1){
+        $('.status').addClass("text-warning");
+    }
+    else if ($('#status-of-order').text().indexOf('Completed') > -1){
+        $('.status').addClass("text-success");
+    }
+    else if ($('#status-of-order').text().indexOf('Comfirmed') > -1){
+        $('.status').addClass("text-info");
+    }
+    else if ($('#status-of-order').text().indexOf('Rejected') > -1){
+        $('.status').addClass("text-danger");
+    }
+});
+
