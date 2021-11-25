1f200454afe8918a4edb5c9d108d0944820b7b77
Shiqi Li <2412230983lsq@gmail.com>
2021-11-25 15:39:46 -0500

    fix rate bugs

--- resume_review/views.py
+++ resume_review/views.py
@@ -219,8 +219,7 @@ class OrderDetailView(FormView):
             rate = self.request.POST.get('rate', '')
             comment = self.request.POST.get('comment', '')
 
-            comment_obj = Comment.objects.create(reviewer=order.reviewer, rate=rate, create_at=datetime.now(
-                pytz.timezone('US/Eastern')), account=account)
+            comment_obj = Comment.objects.create(reviewer=order.reviewer, rate=rate, create_at=datetime.now(), account=account)
 
             if comment:
                 comment_obj.comment = comment
