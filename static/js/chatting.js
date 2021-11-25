5809a291e700cfc1ab5bc2ef6daf9f1072da10e3
txj169 <63174106+txj169@users.noreply.github.com>
2021-11-24 23:56:56 -0500

    chatting  with error message

---
+++ static/js/chatting.js
@@ -0,0 +1,40 @@
+$(document).on('submit', '#post-form', function (e) {
+    e.preventDefault();
+
+    $.ajax({
+        type: 'POST',
+        url: '/send',
+        data: {
+            username: $('#username').val(),
+            room_id: $('#room_id').val(),
+            message: $('#message').val(),
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+        },
+        success: function (data) {
+            //alert(data)
+        }
+    });
+    document.getElementById('message').value = ''
+});
+
+
+$(document).ready(function () {
+
+    setInterval(function () {
+        $.ajax({
+            type: 'GET',
+            url: "/getMessages/{{room}}/",
+            success: function (response) {
+                console.log(response);
+                $("#display").empty();
+                for (var key in response.messages) {
+                    var temp = "<li class='clearfix'><div class= 'message-data text-right'><span class='message-data-time'>" + response.messages[key].date + "</span><img src=" + response.messages[key].user.account.avatar.url + "alt='avatar'></div><div class='message other-message float-right'>" + response.messages[key].value + "</div></li>";
+                    $("#display").append(temp);
+                }
+            },
+            error: function (response) {
+                alert('An error occured')
+            }
+        });
+    }, 1000);
+})
